4e1d6aabec9e24484d7e04927f0c51fc85aafdfb
Edward Tran <edtran@microsoft.com>
2021-09-03 23:05:58 -0700

    Add test to send single message

--- e2e/src/livechat.test.ts
+++ e2e/src/livechat.test.ts
@@ -1,8 +1,31 @@
 import OmnichannelChatSDK from '../../src/OmnichannelChatSDK';
 
 describe("Live Chat", () => {
+    const config = JSON.parse(process.env.CONFIG as string);
+
     it("Read Environment Config", () => {
-        console.log(process.env.CONFIG);
-        console.log("It's working!");
-    })
+        expect(config).toBeTruthy();
+    });
+
+    it("Sending a Single Message", async () => {
+        const chatSDK = new OmnichannelChatSDK({
+            orgId: config.orgId,
+            orgUrl: config.orgUrl,
+            widgetId: config.widgetIds.liveChat
+        }, {
+            telemetry: {
+                disable: true
+            }
+        });
+
+        await chatSDK.initialize();
+
+        await chatSDK.startChat();
+
+        await chatSDK.sendMessage({
+            content: 'Hello!'
+        });
+
+        await chatSDK.endChat();
+    });
 });
